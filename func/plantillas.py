d8367ea4bc3112bd030d1f33b50fae93455ec00f
tmchek <pinanahima@gmail.com>
2025-01-25 19:57:33 -0500

    Add files via upload

---
+++ func/plantillas.py
@@ -0,0 +1,24 @@
+Plantilla_free = '''<b><i>↯ » ↯ 𝐓𝐨𝐤𝐲𝐨 𝐒𝐜𝐫𝐚𝐩𝐩𝐞𝐫 
+
+↯ » card: <code>{}|{}|{}|{}</code>
+
+↯ » Extra: <code>{}</code>
+
+↯  » Bin: <code>#{}</code>
+↯  » Country: <code>{} [{}]</code>
+↯  » Data: <code>{} {} {}</code>
+↯  » Bank:  <code>{}</code></i></b>'''
+
+
+Plantilla_Premium = '''<b><i>↯↯ » ↯ » 𝐓𝐨𝐤𝐲𝐨 𝐒𝐜𝐫𝐚𝐩𝐩𝐞𝐫 𝐋𝐢𝐯𝐞
+
+↯ » card: <code>{}|{}|{}|{}</code>
+↯ » Status: Approved✅
+↯ » Response: <code>{}</code>
+
+↯ » Extra: <code>{}</code>
+
+↯  » Bin: <code>#{}</code>
+↯  » Country: <code>{} [{}]</code>
+↯  » Data: <code>{} {} {}</code>
+↯  » Bank:  <code>{}</code></i></b>'''
