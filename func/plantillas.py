1b5334037dad98f16ae1245bee39665d95782877
tmchek <pinanahima@gmail.com>
2025-04-09 18:51:46 -0400

    Update plantillas.py

--- func/plantillas.py
+++ func/plantillas.py
@@ -1,15 +1,16 @@
-Plantilla_free = '''<b><i> 🥳 » <code>[{}]</code> 🇩🇴𝕺𝖓𝖊 𝖙𝖊𝖈𝖍 scrapper Free
+Plantilla_free = '''<b><i> あ » <code>[{}]</code> TM scrapper Free
 
-⚡ 𝗖𝗰: <code>{}|{}|{}|{}</code>
-⚡ 𝗘𝘅𝘁𝗿𝗮: <code>{}</code>
-⚡ Bin: <code>#{}</code>
-⚡ País: <code>{} [{}]</code>
-⚡ Data: <code>{} {} {}</code>
-⚡ Bank: <code>{}</code></i></b>'''
+↯ » 𝗖𝗰 : <code>{}|{}|{}|{}</code>
+
+↯ » 𝗘𝘅𝘁𝗿𝗮 : <code>{}</code>
+
+↯ » Bin: <code>#{}</code>
+↯ » Pais: <code>{} [{}]</code>
+↯ » Data: <code>{} {} {}</code>
+↯ » Bank:  <code>{}</code></i></b>'''
 
 
-
-Plantilla_Premium = '''<b><i>⚡ » <code>[{}]</code> 🇩🇴𝕺𝖓𝖊 𝖙𝖊𝖈𝖍 scrapper Premium [👑] | 
+Plantilla_Premium = '''<b><i>あ » <code>[{}]</code> TM scrapper Premium [👑] | 
 
 ↯ » 𝗖𝗰 : <code>{}|{}|{}|{}</code>
 ↯ » Status: Approved✅
